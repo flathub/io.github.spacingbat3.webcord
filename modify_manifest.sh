1a3e637d49ec7d63a05d3217e88cf71ecf39d039
axtlos <axtlos@tar.black>
2022-06-06 15:25:20 +0200

    move git stuff to script

--- modify_manifest.sh
+++ modify_manifest.sh
@@ -6,5 +6,9 @@ NEW_SHASUM=$(sha256sum v3.2.0.tar.gz | awk 'BEGIN { FS = " " }; { print $1 }')
 sed -i "s\.*url: https://github.com/vega-d.*\        url: https://github.com${NEW_URL}\g" io.github.spacingbat3.webcord.yml
 sed -i "s\.*sha256:.*\        sha256: ${NEW_SHASUM}\g" io.github.spacingbat3.webcord.yml
 cat io.github.spacingbat3.webcord.yml
-rm latest
-rm $(ls | find -- *.tar.gz | grep v)
+git config user.name "Github manifest bot"
+git config user.email "<>"
+git add io.github.spacingbat3.webcord.yml
+git commit -m "modify manifest with new release and sha256sum"
+git push origin master
+
