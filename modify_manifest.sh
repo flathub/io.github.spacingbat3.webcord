a2879b84519742ef1279653498ffd89170a1ee39
axtlos <axtlos@tar.black>
2022-06-06 15:14:20 +0200

    change ci script

--- modify_manifest.sh
+++ modify_manifest.sh
@@ -6,4 +6,4 @@ NEW_SHASUM=$(sha256sum v3.2.0.tar.gz | awk 'BEGIN { FS = " " }; { print $1 }')
 sed -i "s\.*url: https://github.com/vega-d.*\        url: https://github.com${NEW_URL}\g" io.github.spacingbat3.webcord.yml
 sed -i "s\.*sha256:.*\        sha256: ${NEW_SHASUM}\g" io.github.spacingbat3.webcord.yml
 rm latest
-rm $(ls | find -- *.tar.gz) | grep v
+rm ls | find -- *.tar.gz | grep v
