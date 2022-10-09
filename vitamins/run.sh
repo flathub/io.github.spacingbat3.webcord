2e0bbf61eebaa5ff70fc3b759952df495e4e2ff5
Oro <93224879+orowith2os@users.noreply.github.com>
2022-10-09 16:57:31 -0500

    Do the wayland check

--- vitamins/run.sh
+++ vitamins/run.sh
@@ -6,12 +6,19 @@ socat $SOCAT_ARGS \
     &
 socat_pid=$!
 
-FLAGS='--enable-gpu-rasterization --enable-zero-copy --enable-gpu-compositing --enable-native-gpu-memory-buffers --enable-oop-rasterization --enable-features=UseSkiaRenderer'
+if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
+ FLAGS="--enable-features=WaylandWindowDecorations,UseOzonePlatform,UseSkiaRenderer --ozone-platform=wayland --enable-gpu-rasterization --enable-zero-copy --enable-gpu-compositing --enable-native-gpu-memory-buffers --enable-oop-rasterization --disable-gpu-vsync"
+else
+ FLAGS='--enable-gpu-rasterization --enable-zero-copy --enable-gpu-compositing --enable-native-gpu-memory-buffers --enable-oop-rasterization --enable-features=UseSkiaRenderer'
+fi
+
 
 if [[ $XDG_SESSION_TYPE == "Wayland" ]] && [ -c /dev/nvidia0 ]
 then
     FLAGS="$FLAGS --disable-gpu-sandbox"
 fi
+
+
 
 env TMPDIR=$XDG_CACHE_HOME zypak-wrapper /app/bin/webcord/usr/bin/webcord $FLAGS "$@"
 kill -SIGTERM $socat_pid
