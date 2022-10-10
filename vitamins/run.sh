c813b6170c9ec93b9e77c88c3725b4bb47855fca
axtloss <axtlos@tar.black>
2022-10-10 22:09:54 +0200

    Bump to 3.8.8 and enable flags again

--- vitamins/run.sh
+++ vitamins/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+FLAGS='--enable-gpu-rasterization --enable-zero-copy --enable-gpu-compositing --enable-native-gpu-memory-buffers --enable-oop-rasterization --enable-features=UseSkiaRenderer'
+
 if [[ $XDG_SESSION_TYPE == "Wayland" ]] && [ -c /dev/nvidia0 ]
 then
-    FLAGS="--disable-gpu-sandbox"
+    FLAGS="$FLAGS --disable-gpu-sandbox"
 fi
 
 env TMPDIR=$XDG_CACHE_HOME zypak-wrapper /app/bin/webcord/usr/bin/webcord $FLAGS "$@"
