f626fb0d52388acd81eb48bc07c6c7a5928b6dcb
Hari Rana <theevilskeleton@riseup.net>
2023-02-15 19:05:02 -0500

    Add workaround for UncaughtException

--- vitamins/run.sh
+++ vitamins/run.sh
@@ -6,4 +6,13 @@ then
     FLAGS="$FLAGS --disable-gpu-sandbox"
 fi
 
+# This deletes the windowState file when it contains 0 bytes.
+# Works around https://github.com/flathub/io.github.spacingbat3.webcord/issues/37
+WINDOWSTATE="$XDG_CONFIG_HOME/WebCord/windowState"
+if [[ -f "$WINDOWSTATE" ]]; then
+    if [[ $(wc -c "$WINDOWSTATE" | cut -d' ' -f1) == 0 ]]; then
+        rm "$WINDOWSTATE"
+    fi
+fi
+
 env TMPDIR="$XDG_RUNTIME_DIR/app/${FLATPAK_ID:-io.github.spacingbat3.webcord}" zypak-wrapper /app/bin/webcord/usr/bin/webcord $FLAGS "$@"
