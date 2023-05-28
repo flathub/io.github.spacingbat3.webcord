2a0a9fd2d36f2e988b0a7926c552f1ddcd158a53
axtloss <axtlos@getcryst.al>
2023-05-28 15:39:43 +0200

    Enable wayland CSD when on gnome

--- vitamins/run.sh
+++ vitamins/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 FLAGS='--enable-gpu-rasterization --enable-zero-copy --enable-gpu-compositing --enable-native-gpu-memory-buffers --enable-oop-rasterization --enable-features=UseSkiaRenderer --ozone-platform-hint=auto'
 
+if [[ $XDG_SESSION_TYPE =~ "[Ww]ayland" && $XDG_CURRENT_DESKTOP == "GNOME" ]]
+then
+    FLAGS="$FLAGS --enable-features=WaylandWindowDecorations"
+fi
+
 if [[ $XDG_SESSION_TYPE =~ "[Ww]ayland" ]] && [ -c /dev/nvidia0 ]
 then
     FLAGS="$FLAGS --disable-gpu-sandbox"
